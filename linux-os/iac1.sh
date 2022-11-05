f81fb569f3e72e6f75806f53ee7b5a214ddf76ec
KattsonBastos <kattsonbastos@gmail.com>
2022-11-05 23:35:16 +0000

    Creating IaC for users, groups and permissions

---
+++ linux-os/iac1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Creating Folders!"
+
+mkdir /publico /adm /ven /sec
+
+echo "Creating User groups!"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Creating Users!"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+
+
+echo "Setting folders permissions to users"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Process Finished!"
+
+
